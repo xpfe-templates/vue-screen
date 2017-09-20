32ffe0494c43f3df2c4ce7905a376e797394a5d9
xiaoping <edwardhjp@gmail.com>
2017-09-20 19:54:25 +0800

    Update user api

--- src/mock/user.js
+++ src/mock/user.js
@@ -6,19 +6,21 @@
  * @desc [login mock]
 */
 
-import { param2Obj } from 'utils'
+// import { param2Obj } from 'utils'
 
 const mockUsers = {
   admin: {
     roles: ['admin'],
     token: 'admin',
-    username: '管理员'
+    username: '管理员',
+    avatarUrl: 'https://static.excaliburhan.com/demo/img-test-face.jpg',
   },
   user: {
     roles: ['user'],
-    token: ['user'],
-    username: '游客'
-  }
+    token: 'user',
+    username: '游客',
+    avatarUrl: 'https://static.excaliburhan.com/demo/img-test-face.jpg',
+  },
 }
 
 export default {
@@ -27,36 +29,36 @@ export default {
     if (mockUsers[username]) {
       return {
         code: 0,
-        errMsg: '',
-        data: mockUsers[username]
+        errorMsg: '',
+        data: mockUsers[username],
       }
     }
     return {
       code: -1,
-      errMsg: '用户不存在',
-      data: null
+      errorMsg: '用户不存在',
+      data: null,
     }
   },
   logout: () => {
     return {
       code: 0,
-      errMsg: '',
-      data: null
+      errorMsg: '',
+      data: null,
     }
   },
   getUserInfo: config => {
-    const { token } = param2Obj(config.url)
+    const { token } = JSON.parse(config.body)
     if (mockUsers[token]) {
       return {
         code: 0,
-        errMsg: '',
-        data: mockUsers[token]
+        errorMsg: '',
+        data: mockUsers[token],
       }
     }
     return {
       code: 10004, // TODO，根据实际情况修改
-      errMsg: 'auth fail',
-      data: null
+      errorMsg: 'auth fail',
+      data: null,
     }
   }
 }
