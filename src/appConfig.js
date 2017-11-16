68bc096cf2500944537ae95d2a3de0a6db0b10db
xiaoping <edwardhjp@gmail.com>
2017-11-16 20:29:25 +0800

    修改axios封装方法

--- src/appConfig.js
+++ src/appConfig.js
@@ -2,30 +2,35 @@
  * @author xiaoping
  * @email edwardhjp@gmail.com
  * @create date 2017-10-16 11:16:57
- * @modify date 2017-10-16 11:16:57
+ * @modify date 2017-11-16 08:26:01
  * @desc [配置信息]
 */
 
 const env = process.env.NODE_ENV
 
-const tokenKey = 'screen-token'
-let htmlTitle = 'Vue Screen - dev'
-let baseURL = 'https://www.easy-mock.com/mock/59eff97c7af0b52dd53c60ae/screen'
+const deviceInfo = '' // 应用信息
+const authErrorCode = 3000 // 没有权限的错误码
+const tokenKey = 'screen-token' // 保存的token值
+let htmlTitle = 'Vue Admin - dev'
+let baseURL = 'https://www.easy-mock.com/mock/5a0bf56bdbfe9e4cbd641706/unmanned'
 let authURL = 'https://www.easy-mock.com/mock/59f02babb120c445fab92be2/account'
 
 if (env === 'production') {
-  htmlTitle = 'Vue Screen'
-  baseURL = 'https://www.easy-mock.com/mock/59eff97c7af0b52dd53c60ae/screen'
-  authURL = 'https://www.easy-mock.com/mock/59f02babb120c445fab92be2/account'
+  htmlTitle = 'Vue Admin'
+  baseURL = 'https://api.example.com'
+  authURL = 'https://auth.example.com'
 } else if (env === 'testing') {
-  htmlTitle = 'Vue Screen - test'
-  baseURL = 'https://www.easy-mock.com/mock/59eff97c7af0b52dd53c60ae/screen'
-  authURL = 'https://www.easy-mock.com/mock/59f02babb120c445fab92be2/account'
+  htmlTitle = 'Vue Admin - test'
+  baseURL = 'https://apitest.example.com'
+  authURL = 'https://authtest.example.com'
 }
 
 module.exports = {
+  deviceInfo,
+  authErrorCode,
   tokenKey,
   htmlTitle,
   baseURL,
   authURL,
 }
+
